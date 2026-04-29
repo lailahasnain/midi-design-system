062490e2f858e276d1139859454046ecbe26d1ee
Laila H Aaser <laila.aaser@gmail.com>
2026-04-28 23:54:37 -0700

    Add tokens.js for design tokens configuration

---
+++ src/tokens/tokens.js
@@ -0,0 +1,47 @@
+export const tokens = {
+  color: {
+    brand: {
+      primary400: '#579fff',
+      primary200: '#88b8fb',
+      primary600: '#006af9',
+    },
+    text: {
+      white: '#ffffff',
+      secondary700: '#6b7c88',
+      secondaryHover: '#415465',
+      disabled: '#bdc8cc',
+    },
+    background: {
+      disabled: '#eef5f7',
+      primaryHover: '#e7f0f3',
+    },
+    border: {
+      primary: '#8998a1',
+      disabledSubtle: '#eef5f7',
+    },
+    effects: {
+      shadowXs: 'rgba(10, 13, 18, 0.05)',
+      shadowInnerBorder: 'rgba(10, 13, 18, 0.18)',
+      shadowInner: 'rgba(10, 13, 18, 0.05)',
+      focusRing: '#b6e7a0',
+    },
+  },
+  spacing: {
+    xxs: '2px',
+    xs: '4px',
+    sm: '6px',
+    md: '8px',
+    lg: '12px',
+    xl: '16px',
+  },
+  font: {
+    sizeSm: '14px',
+    sizeMd: '16px',
+    lineHeightSm: '20px',
+    lineHeightMd: '24px',
+  },
+  radius: {
+    sm: '6px',
+    full: '9999px',
+  },
+}
